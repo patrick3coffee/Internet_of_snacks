dd083066a71187bf97fa5af3acaf2cca34b4938c
Patrick Prescott <patrick.prescott@sparkfun.com>
2018-08-06 17:54:53 -0600

    personalized and cleanup examples

--- IoP/IoP.ino
+++ IoP/IoP.ino
@@ -1,6 +1,6 @@
 #include <SparkFunSX1509.h> //Library for the SX1509 IO Expander
 #include <OneWire.h> //Library for the DS18B20 One-wire temp sensor 
-//#include <SparkFunCCS811.h> //Library for the CCS811 Air Quality Sensor
+#include <SparkFunCCS811.h> //Library for the CCS811 Air Quality Sensor
 #include <Wire.h>
 #include <SPI.h>
 #include <Client.h>
@@ -12,18 +12,18 @@
 #include <CayenneMQTTWiFiClient.h>
 #include <DetectDevice.h>
 
+// Keep our secrets out of GitHub
+#include "secrets.h"
+
 // This example shows how to connect to Cayenne using an ESP8266 and send/receive sample data.
-// Make sure you install the ESP8266 Board Package via the Arduino IDE Board Manager and select the correct ESP8266 board before compiling. 
+// Make sure you install the ESP8266 Board Package via the Arduino IDE Board Manager and select the correct ESP8266 board before compiling.
 
 //#define CAYENNE_DEBUG
 #define CAYENNE_PRINT Serial
 
-#if 0 
-//Air Quality Sensor 
+//Air Quality Sensor
 #define CCS811_ADDR 0x5B //Default I2C Address
 CCS811 mySensor(CCS811_ADDR);
-#endif
-
 
 //SX1509 IO Expander
 const byte SX1509_ADDRESS = 0x3E;  // SX1509 I2C address
@@ -40,16 +40,8 @@ int s0 = 4;
 int s1 = 13;
 int s2 = 12;
 int s3 = 16;
-int SIG_pin = A0; 
+int SIG_pin = A0;
 
-// WiFi network info.
-char ssid[] = "sparkfun-guest";
-char wifiPassword[] = "sparkfun6333";
-
-// Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
-char username[] = "601aa000-6fb9-11e7-a036-7dfc05936e4e";
-char password[] = "aab2fbba5ec8b37cb54f1aeccc9e24109fb29e11";
-char clientID[] = "9ca1ebc0-714c-11e7-bc8f-4d8331e13947";
 
 unsigned long lastMillis = 0;
 
@@ -57,32 +49,30 @@ unsigned long lastMillis = 0;
 void setup() {
   Serial.begin(9600);
   Cayenne.begin(username, password, clientID, ssid, wifiPassword);
-  Serial.println("Internet of Plants!!!");
+  Serial.println("Internet of Snacks!!!");
 
- 
+  CCS811Core::status returnCode = mySensor.begin();
+  if (returnCode != CCS811Core::SENSOR_SUCCESS)
+  {
+    Serial.println(".begin() returned with an error.");
+  }
 
-
-  /
-  
 
   //Analog MUX set up:
-  pinMode(s0, OUTPUT); 
-  pinMode(s1, OUTPUT); 
-  pinMode(s2, OUTPUT); 
-  pinMode(s3, OUTPUT); 
+  pinMode(s0, OUTPUT);
+  pinMode(s1, OUTPUT);
+  pinMode(s2, OUTPUT);
+  pinMode(s3, OUTPUT);
 
   digitalWrite(s0, LOW);
   digitalWrite(s1, LOW);
   digitalWrite(s2, LOW);
   digitalWrite(s3, LOW);
-  
+
 }
 
 void loop() {
   Cayenne.loop();
-
-
-
 
   //Get Soil Moisture Readings
   //The Soil Moisture Sensors are powered by the output pin on the IO Expander
@@ -114,7 +104,7 @@ void loop() {
   delay(500);
   io.digitalWrite(SX1509_PWR_SM3, LOW); // Set the I/O low
   Serial.print("SM3:  ");
-  Serial.println(SM3);  
+  Serial.println(SM3);
 
   //Get Soil Moisture Levels from Sensor 4:
   io.digitalWrite(SX1509_PWR_SM4, HIGH);
@@ -125,7 +115,7 @@ void loop() {
   Serial.print("SM4:  ");
   Serial.println(SM4);
 
-  
+
   //Get Soil Moisture Levels from Sensor 5:
   io.digitalWrite(SX1509_PWR_SM5, HIGH);
   delay(500); // Delay half-a-second
@@ -135,47 +125,60 @@ void loop() {
   Serial.print("SM5:  ");
   Serial.println(SM5);
 
-  
+
 
   float adc0 = readMux(0);
-  float volt0 = adc0*0.00097752;
-  float degc0 = (volt0 -0.5)*100.0;
-  float temp1 = degc0 *(9.0/5.0)+32.0;  
+  float volt0 = adc0 * 0.00097752;
+  float degc0 = (volt0 - 0.5) * 100.0;
+  float temp1 = degc0 * (9.0 / 5.0) + 32.0;
   delay(500);
   Serial.print("Temp1:  ");
   Serial.println(temp1);
 
   float adc1 = readMux(1);
-  float volt1 = adc1*0.00097752;
-  float degc1 = (volt1 -0.5)*100.0;
-  float temp2 = degc1 *(9.0/5.0)+32.0;  
+  float volt1 = adc1 * 0.00097752;
+  float degc1 = (volt1 - 0.5) * 100.0;
+  float temp2 = degc1 * (9.0 / 5.0) + 32.0;
   delay(500);
   Serial.print("Temp2:  ");
   Serial.println(temp2);
 
   float adc2 = readMux(2);
-  float volt2 = adc2*0.00097752;
-  float degc2 = (volt2 -0.5)*100.0;
-  float temp3 = degc2 *(9.0/5.0)+32.0;
-  delay(500);  
+  float volt2 = adc2 * 0.00097752;
+  float degc2 = (volt2 - 0.5) * 100.0;
+  float temp3 = degc2 * (9.0 / 5.0) + 32.0;
+  delay(500);
   Serial.print("Temp3:  ");
   Serial.println(temp3);
-    
+
   float adc3 = readMux(3);
-  float volt3 = adc3*0.00097752;
-  float degc3 = (volt3 -0.5)*100.0;
-  float temp4 = degc3 *(9.0/5.0)+32.0;
+  float volt3 = adc3 * 0.00097752;
+  float degc3 = (volt3 - 0.5) * 100.0;
+  float temp4 = degc3 * (9.0 / 5.0) + 32.0;
   delay(500);
   Serial.print("Temp4:  ");
-  Serial.println(temp4);  
-    
+  Serial.println(temp4);
+
   float adc4 = readMux(4);
-  float volt4 = adc4*0.00097752;
-  float degc4 = (volt4 -0.5)*100.0;
-  float temp5 = degc4 *(9.0/5.0)+32.0;
-  delay(500);  
+  float volt4 = adc4 * 0.00097752;
+  float degc4 = (volt4 - 0.5) * 100.0;
+  float temp5 = degc4 * (9.0 / 5.0) + 32.0;
+  delay(500);
   Serial.print("Temp5:  ");
-  Serial.println(temp5);  
+  Serial.println(temp5);
+
+  int light = readMux(10);
+
+  int CO2, TVOC;
+  //Check to see if data is ready with .dataAvailable()
+  if (mySensor.dataAvailable())
+  {
+    //If so, have the sensor read and calculate the results.
+    //Get them later
+    mySensor.readAlgorithmResults();
+    CO2 = mySensor.getCO2();
+    TVOC = mySensor.getTVOC();
+  }
 
   //Publish data every  2 minutes (120000 milliseconds). Change this value to publish at a different interval.
   if (millis() - lastMillis > 120000) {
@@ -192,12 +195,12 @@ void loop() {
     Cayenne.virtualWrite(8, temp3);
     Cayenne.virtualWrite(9, temp4);
     Cayenne.virtualWrite(10, temp5);
-    Cayenne.virtualWrite(11, LIGHT);
-    //Cayenne.virtualWrite(12, readCO2);
-   // Cayenne.virtualWrite(13, readTVOC);
+    Cayenne.virtualWrite(11, light);
+    Cayenne.virtualWrite(12, CO2);
+    Cayenne.virtualWrite(13, TVOC);
   }
 
- 
+
 }
 
 //Default function for processing actuator commands from the Cayenne Dashboard.
@@ -207,30 +210,30 @@ CAYENNE_IN_DEFAULT()
   CAYENNE_LOG("CAYENNE_IN_DEFAULT(%u) - %s, %s", request.channel, getValue.getId(), getValue.asString());
   //Process message here. If there is an error set an error message using getValue.setError(), e.g getValue.setError("Error message");
 }
-int readMux(int channel){
+int readMux(int channel) {
   int controlPin[] = {s0, s1, s2, s3};
 
-  int muxChannel[16][4]={
-    {0,0,0,0}, //channel 0
-    {1,0,0,0}, //channel 1
-    {0,1,0,0}, //channel 2
-    {1,1,0,0}, //channel 3
-    {0,0,1,0}, //channel 4
-    {1,0,1,0}, //channel 5
-    {0,1,1,0}, //channel 6
-    {1,1,1,0}, //channel 7
-    {0,0,0,1}, //channel 8
-    {1,0,0,1}, //channel 9
-    {0,1,0,1}, //channel 10
-    {1,1,0,1}, //channel 11
-    {0,0,1,1}, //channel 12
-    {1,0,1,1}, //channel 13
-    {0,1,1,1}, //channel 14
-    {1,1,1,1}  //channel 15
+  int muxChannel[16][4] = {
+    {0, 0, 0, 0}, //channel 0
+    {1, 0, 0, 0}, //channel 1
+    {0, 1, 0, 0}, //channel 2
+    {1, 1, 0, 0}, //channel 3
+    {0, 0, 1, 0}, //channel 4
+    {1, 0, 1, 0}, //channel 5
+    {0, 1, 1, 0}, //channel 6
+    {1, 1, 1, 0}, //channel 7
+    {0, 0, 0, 1}, //channel 8
+    {1, 0, 0, 1}, //channel 9
+    {0, 1, 0, 1}, //channel 10
+    {1, 1, 0, 1}, //channel 11
+    {0, 0, 1, 1}, //channel 12
+    {1, 0, 1, 1}, //channel 13
+    {0, 1, 1, 1}, //channel 14
+    {1, 1, 1, 1} //channel 15
   };
 
   //loop through the 4 sig
-  for(int i = 0; i < 4; i ++){
+  for (int i = 0; i < 4; i ++) {
     digitalWrite(controlPin[i], muxChannel[channel][i]);
   }
 
---
+++ IoP/secrets-template.h
@@ -0,0 +1,13 @@
+/*
+ *  change the values below and rename the file to secrets.h
+ */
+
+
+// WiFi network info.
+char ssid[] = "wifi name";
+char wifiPassword[] = "wifi password";
+
+// Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
+char username[] = "cayenne username string";
+char password[] = "cayenne password string";
+char clientID[] = "cayenne clientID string";
